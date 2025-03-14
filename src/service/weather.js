dfbaaf7d2be6a8f0bd10c386c202168d571e6393
Jatin Rana <jatinrana120@gmail.com>
2025-03-14 18:54:53 +0530

    http changed to https

--- src/service/weather.js
+++ src/service/weather.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const weather_api = createApi({
     reducerPath: 'weather_api',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://api.weatherapi.com/v1' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://api.weatherapi.com/v1' }),
     endpoints: (builder) => ({
         getWeatherData: builder.query({
             query: (params = {}) => ({
